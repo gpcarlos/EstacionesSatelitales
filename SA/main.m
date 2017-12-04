635acac7a83ec0c124fdcdefe3a7b5365e2285a7
Carlos Gallardo Polanco <carlosgallardopolanco@gmail.com>
2017-12-04 20:38:54 +0100

    SA fixed

--- SA/main.m
+++ SA/main.m
@@ -5,6 +5,8 @@ M=40; % Número de representantes
 rand('seed',50);
 space = randi(N,N,2); % coordenadas de los satélites en el espacio
 
+
+
 [X,C,i]=simulatedAnnealing(N,M,space)
 
 plot(space(:,1),space(:,2),'.'); hold on;
