cb9179c3cae5a3dd8712246aaf27d9955370589c
aaronariasperez <33655005+aaronariasperez@users.noreply.github.com>
2017-12-11 22:06:24 +0100

    a
    minor changes

--- SA/Fvalue.m
+++ SA/Fvalue.m
@@ -1,6 +1,6 @@
 % Devuelve el valor total de las distancias de los representantes actuales
 
-function valor = Fvalue(current, coordinates)
+function value = Fvalue(current, coordinates)
     dist = distToRep(current, coordinates); % Calculamos las distancias
-    valor = sum(min(dist')); % Sumamos solo las distancias de los representantes
+    value = sum(min(dist')); % Sumamos solo las distancias de los representantes
 end
